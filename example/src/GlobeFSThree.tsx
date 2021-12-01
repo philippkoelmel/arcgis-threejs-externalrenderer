a36e7b98766d99ac9cd9523e37df48c7dd48e556
Philipp Kölmel <me@philippkoelmel.com>
2021-12-01 22:37:26 +0000

    put the box on ground

--- example/src/GlobeFSThree.tsx
+++ example/src/GlobeFSThree.tsx
@@ -1,5 +1,5 @@
 import * as externalRenderers from "@arcgis/core/views/3d/externalRenderers";
-import { WebGLRenderer, Scene, BoxGeometry, Mesh, Clock, MathUtils, MeshPhongMaterial, Material, BufferGeometry } from "three";
+import { WebGLRenderer, Scene, Matrix4, BoxGeometry, Mesh, Clock, MathUtils, MeshStandardMaterial, Material, BufferGeometry } from "three";
 import SpatialReference from "@arcgis/core/geometry/SpatialReference"
 import { ExternalRendererThreeCallback } from "@philippkoelmel/arcgis-threejs-externalrenderer";
 
@@ -11,34 +11,33 @@ class GlobeFSThree implements ExternalRendererThreeCallback {
     private radiansPerSecond: number = MathUtils.degToRad(30);;
 
     setup(context: any, renderer: WebGLRenderer, scene: Scene): void {
-        let geometry = new BoxGeometry(10000, 10000, 10000);
-        let material = new MeshPhongMaterial({
+        let geometry = new BoxGeometry(100, 100, 100);
+        let material = new MeshStandardMaterial({
             color: 0xFF0000,
             flatShading: true,
         });
 
         this.mesh = new Mesh(geometry, material);
-        this.mesh.scale.set(10, 10, 10);
         scene.add(this.mesh);
 
-        let posEst = [9.993, 53.679, 400000];
-        let renderPos = [0, 0, 0];
-        externalRenderers.toRenderCoordinates(context.view, posEst, 0, SpatialReference.WGS84, renderPos, 0, 1);
-
-        this.mesh.position.set(renderPos[0], renderPos[1], renderPos[2]);
+        let WGS84Position = [9.993, 53.679, 0];
 
         context.view.goTo({
-            target: [posEst[0], posEst[1]],
-            zoom: 5,
+            target: [WGS84Position[0], WGS84Position[1]],
+            zoom: 14,
         });
+
+        let transformation = new Array(16);
+        externalRenderers.renderCoordinateTransformAt(context.view, WGS84Position, SpatialReference.WGS84, transformation);
+        this.mesh.applyMatrix4(new Matrix4().fromArray(transformation));
     }
 
     render(context: any): void {
         const clockDelta = this.clock.getDelta();
 
-        this.mesh!.rotation.x += this.radiansPerSecond * clockDelta;
-        this.mesh!.rotation.y += this.radiansPerSecond * clockDelta;
-        this.mesh!.rotation.z += this.radiansPerSecond * clockDelta;
+        //this.mesh!.geometry.rotateX(this.radiansPerSecond * clockDelta);
+        this.mesh!.geometry.rotateY(this.radiansPerSecond * clockDelta);
+        //this.mesh!.geometry.rotateZ(this.radiansPerSecond * clockDelta);
     }
 
     dispose(context: any): void {
